88520136f76765cf8ae25383d690accbecb719ff
barryz <barryzxb@gmail.com>
2018-03-26 20:44:02 +0800

    防止大家暴力膜伤身，加入了一点限制:trollface:

--- stream.go
+++ stream.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"log"
 	"io/ioutil"
-	"time"
-	"strings"
+	"log"
+	"net/http"
 	"strconv"
+	"strings"
+	"time"
 )
 
 const MAX_FRAME = 360
+
 var frames []string = make([]string, 0, MAX_FRAME)
 
 func leftPad2Len(s string, padStr string, overallLen int) string {
@@ -21,28 +22,27 @@ func leftPad2Len(s string, padStr string, overallLen int) string {
 }
 
 func plus1s(w http.ResponseWriter, r *http.Request) {
-	if !strings.Contains(r.Header.Get("User-Agent"), "curl"){
+	if !strings.Contains(r.Header.Get("User-Agent"), "curl") {
 		http.Redirect(w, r, "https://github.com/HFO4/plus1s.live", http.StatusFound)
-		return 
+		return
 	}
-	i :=1
+	i := 1
 	loop := 0
-	for{
-		if i==MAX_FRAME{
-			i=0
-			loop=loop+1
+	for {
+		if i == MAX_FRAME {
+			i = 0
+			loop++
 		}
-		if loop>20{
-			write(w,"You've waste too much time, we have to stop you.")
+		if loop > 20 {
+			write(w, "You've waste too much time, we have to stop you.")
 			return
 		}
-		write(w,frames[i])
-		i=i+1
+		write(w, frames[i])
+		i++
 	}
 }
 
-
-func write(w http.ResponseWriter,s string){
+func write(w http.ResponseWriter, s string) {
 	flusher, ok := w.(http.Flusher)
 	if !ok {
 		panic("Expected http.ResponseWriter to be an http.CloseNotifier")
@@ -53,21 +53,21 @@ func write(w http.ResponseWriter,s string){
 	flusher.Flush()
 }
 
-func prepare(){
+func prepare() {
 	fmt.Println("Prepare framses...")
-	for i:=0;i<MAX_FRAME;i=i+1{
-		b, err := ioutil.ReadFile("pic/"+leftPad2Len(strconv.Itoa(i+1)  , "0", 3) +".txt")
+	for i := 0; i < MAX_FRAME; i++ {
+		b, err := ioutil.ReadFile("pic/" + leftPad2Len(strconv.Itoa(i+1), "0", 3) + ".txt")
 		if err != nil {
 			fmt.Print(err)
 		}
 		str := string(b)
-		frames = append(frames,str);
+		frames = append(frames, str)
 	}
 	fmt.Println("Framses are ready.")
 }
 
 func main() {
-	prepare();
+	prepare()
 	fmt.Println("Ready to start server")
 	http.HandleFunc("/", plus1s)
 	err := http.ListenAndServe(":1926", nil)
