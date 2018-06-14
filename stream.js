c8e8f6373a1e9c184588eb812d6d99721ae48dec
guoyq <guoyq@wy.cn>
2018-06-14 16:17:06 +0800

    '一点小小的'

--- stream.js
+++ stream.js
@@ -3,38 +3,54 @@ const fs = require("fs");
 
 const MAX_FRAME = 360;
 let frames = new Array(MAX_FRAME);
-
+let i = 1;
 function plus1s(req, res, next) {
-    if (!req.header("User-Agent").includes("curl")) {
-        res.redirect("https://github.com/HFO4/plus1s.live");
-        return;
-    }
-    res.writeHead(200, {
+    if (i == MAX_FRAME) {
+        i = 0;
+    }    
+    res.set({
         'Content-Type': 'text/plain; charset=utf-8',
-        'Transfer-Encoding': 'chunked',
-        'X-Content-Type-Options': 'nosniff'});
-    let i = 1;
-    let loop = 0;
-    var io = setInterval(function () {
-        if (i == MAX_FRAME) {
-            i = 0;
-            loop++;
-        }
-        write(res, frames[i]);
-        i++;
-        if (loop >= 20) {
-            res.end("\nYou've waste too much time, we have to stop you.\n");
-            clearInterval(io);
-        }
-    }, 100);
-    if (next) {
-        next();
-    }
+        'Access-Control-Allow-Origin':'*'});
+    res.send(frames[i]);
+    i++;
+
+    // if (!req.header("User-Agent").includes("curl")) {
+    //     res.redirect("https://github.com/HFO4/plus1s.live");
+    //     return;
+    // }
+    // res.writeHead(200, {
+    //     'Content-Type': 'text/plain; charset=utf-8',
+    //     'Transfer-Encoding': 'chunked',
+    //     'X-Content-Type-Options': 'nosniff',
+    //     'Access-Control-Allow-Origin':'*'});
+    // let i = 1;
+    // let loop = 0;
+    // var io = setInterval(function () {
+    //     if (i == MAX_FRAME) {
+    //         i = 0;
+    //         loop++;
+    //     }
+    //     write(res, frames[i]);
+    //     i++;
+    //     if (loop >= 20) {
+    //         res.end("\nYou've waste too much time, we have to stop you.\n");
+    //         clearInterval(io);
+    //     }
+    // }, 100);
+    // if (next) {
+    //     next();
+    // }
 }
 
 function write(res, str) {
     res.write(str);
     res.write("\033[2J\033[H");    
+}
+
+function index(req,res,next){
+    res.set({'Access-Control-Allow-Origin':'*'});
+    fileName =  __dirname + '/moha.html';
+    res.sendFile(fileName);
 }
 
 function prepare() {
@@ -52,7 +68,8 @@ function main() {
     prepare();
     console.log("Ready to start server");
     let app = express();
-    app.use('/', plus1s);
+    app.use('/moha', plus1s);
+    app.use('/', index)
     app.listen(1926).on('error', console.log);
 }
 
